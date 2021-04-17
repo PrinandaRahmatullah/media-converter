150ab97baaeedce55f386077e6f3cd7117737ed7
Prinanda Rahmatullah <prinanda50@gmail.com>
2021-04-17 23:51:58 +0700

    add converter ogg2mp3

--- audioconverter/audioconverter.py
+++ audioconverter/audioconverter.py
@@ -4,7 +4,7 @@ from pydub.playback import play
 
 
 # Output directory
-OUT_DIR = "../output/audio"
+OUT_DIR = "output/audio"
 os.system(f"mkdir {OUT_DIR}")
 
 
@@ -12,7 +12,18 @@ def Audio2WAV(input_file, output_type="wav"):
     try:
         song = AudioSegment.from_file(
             input_file, format=f"{input_file.split('.')[-1]}")
+        song = song.set_channels(1)
+        song = song.set_frame_rate(16000)
     except:
         print("Failed")
     song.export(f"{OUT_DIR}/{input_file.split('.')[0]}.{output_type}",
-                format=f"{output_type}")
+                format=f"{output_type}", bitrate="256k")
+
+
+def OGG2MP3(input_file, output_type="mp3"):
+    song = AudioSegment.from_ogg(
+        input_file)
+    song = song.set_channels(1)
+    song = song.set_frame_rate(16000)
+    song.export(f"{OUT_DIR}/{input_file.split('.')[0]}.{output_type}",
+                format=output_type, bitrate="256k")
